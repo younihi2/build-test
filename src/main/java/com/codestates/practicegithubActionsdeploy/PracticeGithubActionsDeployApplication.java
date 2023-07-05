9541a07710dad91fb2a99dd3758a21220bf414da
(Jiyoon) <(jiyooon1118@gmail.com)>
2023-07-06 08:37:02 +0900

    Feat: first commit

---
+++ src/main/java/com/codestates/practicegithubActionsdeploy/PracticeGithubActionsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActionsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubActionsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubActionsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicegithubActionsdeploy/PracticeGithubActionsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActionsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
